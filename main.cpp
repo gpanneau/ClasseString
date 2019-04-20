86bebcd56bd1ef61ff6d41f6146c1bff400e0d52
m-luka <luk4mat@gmail.com>
2019-04-20 17:14:10 +0200

    Add test for operator+(string)

--- main.cpp
+++ main.cpp
@@ -19,14 +19,16 @@ int main(int argc, char* argv[]){
 'p','q','s','d','f','g','h','j','k','l','m','w','x','c','v','b','n'};
   string str3(table3);
   std::cout << str3.c_str() << std::endl << str3.size() << std::endl <<str3.capacity() << std::endl << str3.max_size() << std::endl;
-  
+  //reserve
   std::cout << std::endl << std::endl;
   std::cout << str1.c_str() << std::endl << str1.size() << std::endl <<str1.capacity() << std::endl << str1.max_size() << std::endl;
   str1.reserve(25);
   std::cout << str1.c_str() << std::endl << str1.size() << std::endl <<str1.capacity() << std::endl << str1.max_size() << std::endl;
-  
+  //operator=(char*)
   std::cout << std::endl << std::endl;
   str2 = str1.c_str();
   std::cout << str2.c_str() << std::endl << str2.size() << std::endl <<str2.capacity() << std::endl << str2.max_size() << std::endl;
+  //operator+(string)
+  std::cout << std::endl << (str1+str1).c_str() << std::endl;
   return 0;
 }
